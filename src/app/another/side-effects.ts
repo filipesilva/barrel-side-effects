25ae33eca223563355a9c85a70fd9972fda48fbe
Filipe Silva <filipematossilva@gmail.com>
2020-01-31 20:55:43 +0000

    add repro for https://github.com/angular/angular-cli/issues/16799

---
+++ src/app/another/side-effects.ts
@@ -0,0 +1,4 @@
+import { DateTime } from 'luxon';
+
+export const local = () => DateTime.local();
+export const map = new Map([['key', 'value']]);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AnotherModule } from './another/index';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AnotherModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
